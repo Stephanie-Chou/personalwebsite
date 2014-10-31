3696816b92ba1882687c22841cd105144cfa5d1c
Stephanie-Chou <stephanie.chou@outlook.com>
2014-10-31 13:37:13 -0700

    article and tags models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ActiveRecord::Base
+  has_many :article_tags
+  has_many :tags, :through => :article_tags
+
+  def tag(input)
+    tags = input.split(',')
+    tags.each do |tag|
+      new_tag = Tag.find_or_create_by(name:tag)
+      ArticleTag.find_or_create_by(tag:new_tag, article: self)
+    end
+  end
 end
