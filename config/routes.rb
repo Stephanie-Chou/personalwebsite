12d647ac4eb23427a4ef6b747eae8b1f6b7fb3d4
Stephanie-Chou <stephanie.chou@outlook.com>
2014-12-05 16:58:51 -0800

    paintfill

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
 
   get 'error' => 'application#error'
 
+  get 'paintfill' => 'application#paintfill'
+
   resources :posts
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
